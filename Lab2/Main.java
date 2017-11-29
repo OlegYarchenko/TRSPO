f5b9fc8283096bb4d9abf9adebc413658e10a096
OlegYarchenko <34108156+OlegYarchenko@users.noreply.github.com>
2017-11-30 00:13:58 +0200

    Update Main.java

--- Lab2/Main.java
+++ Lab2/Main.java
@@ -1,8 +1,6 @@
 package Lab2;
 
-/**
- * Created by Nick Batist on 10/23/17.
- */
+
 public class Main {
     public static void main(String[] args) {
         Car nissan = new Car(Manufacturer.NISSAN, "Leaf", FuelType.ELECTRICITY);
