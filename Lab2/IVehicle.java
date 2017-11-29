5ba34fc3a83244052216a31b442b54f447dc6836
OlegYarchenko <34108156+OlegYarchenko@users.noreply.github.com>
2017-11-30 00:13:39 +0200

    Update IVehicle.java

--- Lab2/IVehicle.java
+++ Lab2/IVehicle.java
@@ -1,8 +1,6 @@
 package Lab2;
 
-/**
- * Created by Nick Batist on 10/23/17.
- */
+
 public interface IVehicle {
   //  void fillTank(int fuelAmount, FuelType fuelType);
     FuelType getFuelType ();
