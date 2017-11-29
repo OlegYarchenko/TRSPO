b3385426687a9e3fed7395d9d25d81e4e4049a40
OlegYarchenko <34108156+OlegYarchenko@users.noreply.github.com>
2017-11-30 00:11:49 +0200

    Update Main.java

--- Lab1/Main.java
+++ Lab1/Main.java
@@ -2,9 +2,6 @@ package Lab1;
 
 import java.util.Scanner;
 
-/**
- * Created by Nick Batist on 11/23/17.
- */
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
